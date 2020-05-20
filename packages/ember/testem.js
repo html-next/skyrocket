58ac933878308be2bc32a6e2ce62f1ddf5b5d091
Chris Thoburn <runspired@gmail.com>
2020-05-20 16:28:26 -0700

    fix syntax

--- packages/ember/testem.js
+++ packages/ember/testem.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line no-console
+console.log(`\n\nLaunching with ${process.env.TESTEM_CI_LAUNCHER || 'Chrome'}n\n`);
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [process.env.TESTEM_CI_LAUNCHER || 'Chrome'],
   launch_in_dev: ['Chrome'],
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
       ci: [
