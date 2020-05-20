512bc6733a1649eb87ff500c1e16a4e07257fed3
Chris Thoburn <runspired@gmail.com>
2020-05-20 15:19:13 -0700

    tests: target all the things

--- packages/ember/testem.js
+++ packages/ember/testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
+  launch_in_ci: [process.env.TESTEM_CI_LAUNCHER || 'Chrome'],
   launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
