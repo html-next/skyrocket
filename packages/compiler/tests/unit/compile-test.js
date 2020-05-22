1d2ee2c25f9641c05709fdac5aca82adde6ab5ea
Chris Thoburn <runspired@gmail.com>
2020-05-22 11:55:28 -0700

    improve compilation tests

--- packages/compiler/tests/unit/compile-test.js
+++ packages/compiler/tests/unit/compile-test.js
@@ -5,12 +5,14 @@ const QUnit = require('qunit');
 
 const fixtureBuilder = require('../helpers/fixture-builder');
 const setupTest = require('../helpers/setup');
-const iterate = require('../helpers/iterate');
+const iterateDirectoryFiles = require('../helpers/iterate');
 
 const { module: testModule, test } = QUnit;
 
 function collect(rootPath, cache) {
-  iterate(rootPath, function(fullPath, segments) {
+  let allPaths = [];
+  iterateDirectoryFiles(rootPath, function(fullPath, segments) {
+    allPaths.push(fullPath.replace(rootPath, ''));
     let lastSegment;
     let currentCache = cache;
     let lastCache;
@@ -23,6 +25,28 @@ function collect(rootPath, cache) {
 
     lastCache[lastSegment] = fs.readFileSync(fullPath, { encoding: 'utf-8' });
   });
+  return allPaths;
+}
+
+function iterateObjectValues(obj, callback, segments = []) {
+  if (typeof obj === 'string') {
+    callback(obj, segments);
+  } else {
+    Object.keys(obj).forEach(key => {
+      iterateObjectValues(obj[key], callback, [...segments, key]);
+    });
+  }
+}
+
+function get(obj, segments) {
+  let value = obj;
+  for (let segment of segments) {
+    if (!Object.prototype.hasOwnProperty.call(value, segment)) {
+      return undefined;
+    }
+    value = value[segment];
+  }
+  return value;
 }
 
 testModule('Schema Compilation', async function(hooks) {
@@ -30,27 +54,35 @@ testModule('Schema Compilation', async function(hooks) {
   let builder;
   let expectedArtifacts = {};
   let builtArtifacts = {};
+  let expectedFiles;
+  let builtFiles;
 
   hooks.before(async function() {
     builder = fixtureBuilder();
-    collect(path.join(__dirname, '../fixtures/output/'), expectedArtifacts);
+    expectedFiles = collect(path.join(__dirname, '../fixtures/output/'), expectedArtifacts);
     await builder.build();
-    collect(path.join(builder.outputPath, ''), builtArtifacts);
+    builtFiles = collect(path.join(builder.outputPath, '/'), builtArtifacts);
   });
 
   hooks.after(async function() {
     await builder.discard();
   });
 
-  test('We arent missing stuff', async function(assert) {
-    assert.deepEqual(builtArtifacts, expectedArtifacts, 'We built only what we expected');
+  test('We have only the expected files', async function(assert) {
+    assert.deepEqual(builtFiles, expectedFiles, 'We built only what we expected');
   });
 
   test('We compiled optimized schema artifacts correctly', async function(assert) {
-    assert.deepEqual(builtArtifacts.schemas, expectedArtifacts.schemas, 'We built only what we expected');
+    iterateObjectValues(builtArtifacts.schemas, function(value, segments) {
+      let matchingValue = get(expectedArtifacts.schemas, segments);
+      assert.strictEqual(value, matchingValue, `schemas.${segments.join('.')} is what we expected`);
+    });
   });
 
   test('We compiled worker scripts correctly', async function(assert) {
-    assert.deepEqual(builtArtifacts.workers, expectedArtifacts.workers, 'We built only what we expected');
+    iterateObjectValues(builtArtifacts.workers, function(value, segments) {
+      let matchingValue = get(expectedArtifacts.workers, segments);
+      assert.strictEqual(value, matchingValue, `workers.${segments.join('.')} is what we expected`);
+    });
   });
 });
